38429352a9e5fa87bd5cc75191769f34f72c4091
Palmanius <hrdy@gsedu.sk>
2021-09-28 14:45:28 +0200

    Player added
    sprite + animation

--- FirstGame/main.py
+++ FirstGame/main.py
@@ -1,4 +1,7 @@
-import pygame
+import pygame, sys
+from player import Player
+
+
 WIDTH, HEIGHT = 900, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Space Game")
@@ -6,10 +9,16 @@ WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 FPS = 60
 
+moving_sprites = pygame.sprite.Group()
+player = Player(100,100)
+moving_sprites.add(player)
 
 def redraw_window():
     WIN.fill(WHITE)
+    moving_sprites.draw(WIN)
+    moving_sprites.update()
     pygame.display.update()
+
 
 
 def main():
@@ -21,6 +30,12 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_w:
+                    player.animate()
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_w:
+                    player.stop_animating()
         redraw_window()
 
     pygame.quit()
---
+++ FirstGame/player.py
@@ -0,0 +1,37 @@
+import pygame, os
+class Player(pygame.sprite.Sprite):
+    def __init__(self,pos_x,pos_y,health = 100,shields = 100):
+        super().__init__()
+        """ self.image = pygame.Surface([16,16])
+        self.image.fill((0,0,0))
+        self.rect = self.image.get_rect()
+        self.rect.topleft = [pos_x,pos_y] """
+        self.sprites = []
+        self.is_animating = False
+        self.size_x = 50
+        self.size_y = 50
+        self.animation_speed = 0.2
+        Spaceship_image = pygame.image.load(os.path.join("FirstGame\\Assets","SpaceShip (moving1).png"))
+        self.sprites.append(pygame.transform.scale(Spaceship_image,(self.size_x,self.size_y)))
+        Spaceship_image = pygame.image.load(os.path.join("FirstGame\\Assets","SpaceShip (moving2).png"))
+        self.sprites.append(pygame.transform.scale(Spaceship_image,(self.size_x,self.size_y)))
+        Spaceship_image = pygame.image.load(os.path.join("FirstGame\\Assets","SpaceShip (moving3).png"))
+        self.sprites.append(pygame.transform.scale(Spaceship_image,(self.size_x,self.size_y)))
+
+        self.current_sprite = 0
+        self.image = self.sprites[self.current_sprite]
+
+        self.rect = self.image.get_rect()
+        self.rect.topleft = [pos_x,pos_y]
+    def animate(self):
+        self.is_animating = True
+    def stop_animating(self):
+        self.is_animating = False
+    def update(self):
+        if self.is_animating == True:
+            self.current_sprite += self.animation_speed
+            if self.current_sprite >= len(self.sprites):
+                self.current_sprite = 0
+            self.image = self.sprites[int(self.current_sprite)]
+        else:
+            self.image = self.sprites[0]
