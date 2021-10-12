a67c8b5f294d83a5bef0dbda3ce3e30075854956
Pepek-Namornik <92086955+Pepek-Namornik@users.noreply.github.com>
2021-10-12 13:41:41 +0200

    controls
    controls
    pepe namornik

--- FirstGame/controlls.py
+++ FirstGame/controlls.py
@@ -1,5 +1,14 @@
 import pygame,os
+vel=5
 def controls(player):
+    
     keys = pygame.key.get_pressed()
-    player.pos_x += (keys[pygame.K_RIGHT] - keys[pygame.K_LEFT]) * 5
-    player.pos_y += (keys[pygame.K_DOWN] - keys[pygame.K_UP]) * 5
+    if keys[pygame.K_w] and player.pos_y > 0:
+        player.pos_y -= vel
+    elif keys[pygame.K_s] and 450 > player.pos_y:
+        player.pos_y += vel
+    if keys[pygame.K_a] and player.pos_x > 0:
+        player.pos_x -= vel
+    elif keys[pygame.K_d] and 850 > player.pos_x:
+        player.pos_x += vel
+    
